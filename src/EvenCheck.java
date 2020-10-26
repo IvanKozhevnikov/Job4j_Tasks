8bcdd0b26e95dc83a1501f07a380385ca53d090c
Ivan Kozhevnikov <xx71@mail.ru>
2020-10-26 17:21:09 +0300

    Change1

---
+++ src/EvenCheck.java
@@ -0,0 +1,10 @@
+public class EvenCheck {
+    public static void main(String[] args) {
+        System.out.println(check(3));
+    }
+    public static boolean check(int num) {
+        if ((num % 2) == 0)
+            return true;
+        return false;
+    }
+}
